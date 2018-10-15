010306a50d2d417563768c442c5e3f39a5b0cba1
Florian Merle <merle.florian.84@gmail.com>
2018-10-15 07:09:06 +0200

    functional generic player and test flux added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var fluxList = [
+  {hipHop: "http://streamingads.hotmixradio.fm:8000/hotmixradio-hiphop-128.mp3"}
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Radioproject' });
+  res.render('index', { fluxList });
 });
 
 module.exports = router;
